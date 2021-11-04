3d5cf53933614db48b7af61508281d6b565715f8
Atos do Amaral <atosamaral@gmail.com>
2021-11-04 16:14:23 -0300

    Escrevendo Ola mundo

---
+++ PROJETO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace PROJETO
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
